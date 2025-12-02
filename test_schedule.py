4aeb3deb5edc621dab7647deedd2111a00f5b55d
Jayce <jstephe3@xula.edu>
2025-12-01 18:13:39 -0600

    Create test for add_course function

--- test_schedule.py
+++ test_schedule.py
@@ -1,4 +1,14 @@
 import unittest
-from schedule import Schedule
+from schedule import Schedule, load_course_from_csv
 
-class TestSchedule:
+class TestSchedule(unittest.TestCase):
+
+    def test_add_course(self):
+        s = Schedule("001", "Austin")
+        course = {"crn": "10002", "title": "Math"}
+
+        s.add_course(course)
+
+        self.assertEqual(len(s.courses), 1)
+        self.assertEqual(s.courses[0]["crn"], "10002")
+    
